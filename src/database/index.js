1beb0cd2685bd813d5a5d573ed62338a7acea31b
kunalnishad115 <nishadkunal1234567@gmail.com>
2024-08-22 23:26:46 +0530

    utils code discussion

---
+++ src/database/index.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+import { Database_name } from "../constants.js";
+const connectDB= async ()=>{
+   try {
+    const connectionInstance= await mongoose.connect(`${process.env.MONGODB_URI}/${Database_name}`)
+    console.log(` \n MONGODB CONNECTED ${connectionInstance.connection.host}`)
+   } catch (error) {
+    console.error("ERROR DUE TO MONGODB",error)
+    throw error
+    process.exit(1);
+   }
+} 
+export default connectDB
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import dotenv from 'dotenv'
+import connectDB from './database/index.js'
+dotenv.config({
+    path: './env'
+})
+connectDB()
